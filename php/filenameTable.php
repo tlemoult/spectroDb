5b1d4042b137dfc843231e2c85e87096c3384d71
Thierry Lemoult <thierry.lemoult@eileo.com>
2017-01-18 23:41:04 +0100

    three separate table

--- php/filenameTable.php
+++ php/filenameTable.php
@@ -21,20 +21,22 @@ if(empty($_GET['obsId']))
 }
 else
 {
-	echo "<H2>Raw files associated to observation Id=".$_GET['obsId']."</H2>";
+
+    echo "<H1>Observation Id=".$_GET['obsId']."</H2>";
+
+	echo "<H2>Raw files</H2>";
 
 }
 
 // Attempt select query execution
-$sql = "select * from fileName where obsId=".$_GET['obsId']." ORDER by date";
+$sql = "select * from fileName where obsId=".$_GET['obsId']." AND phase='RAW'ORDER by date,filetype,filename";
 
-echo $sql;
+//echo $sql;
 
 if($result = mysqli_query($link, $sql)){
 	
 	$rowcount=mysqli_num_rows($result);
 	
-	echo "<br>".$rowcount." files entry.<br>";
 		
     if($rowcount > 0){
 		
@@ -44,6 +46,7 @@ if($result = mysqli_query($link, $sql)){
   				echo "<th>fileId</th>";
                 echo "<th>phase</th>";
                 echo "<th>filetype</th>";
+                echo "<th>filename</th>";
                 echo "<th>serieId</th>";         
                 echo "<th>date</th>";
                 echo "<th>tempCCD(°C)</th>";
@@ -55,6 +58,7 @@ if($result = mysqli_query($link, $sql)){
                 echo "<td>" . $row['fileId'] . "</td>";				
                 echo "<td>" . $row['phase'] . "</td>";
                 echo "<td>" . $row['filetype'] . "</td>";
+                echo "<td>" . $row['filename'] . "</td>";
                 echo "<td>" . $row['serieId'] . "</td>";
                 echo "<td>" . $row['date'] . "</td>";
                 echo "<td>" . $row['tempCCD'] . "</td>";
@@ -64,6 +68,8 @@ if($result = mysqli_query($link, $sql)){
         }
         echo "</table>";
         echo '</form>';
+        echo $rowcount." files.<br>";
+
         // Close result set
         mysqli_free_result($result);
     } else{
@@ -73,14 +79,65 @@ if($result = mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
 
-echo "<h2>Reduced spectrums associated</h2>";
+echo "<H2>Other files </H2>";
+
+// Attempt select query execution
+$sql = "select * from fileName where obsId=".$_GET['obsId']." AND NOT phase='RAW'  ORDER by date,filetype,filename";
+
+//echo $sql;
+
+if($result = mysqli_query($link, $sql)){
+    
+    $rowcount=mysqli_num_rows($result);
+        
+    if($rowcount > 0){
+        
+        
+        echo "\n<table border=2>";
+            echo "<tr>";
+                echo "<th>fileId</th>";
+                echo "<th>phase</th>";
+                echo "<th>filetype</th>";
+                echo "<th>filename</th>";
+                echo "<th>serieId</th>";         
+                echo "<th>date</th>";
+                echo "<th>tempCCD(°C)</th>";
+                echo "<th>binning</th>";
+                echo "<th>detector</th>";            
+            echo "</tr>\n";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['fileId'] . "</td>";             
+                echo "<td>" . $row['phase'] . "</td>";
+                echo "<td>" . $row['filetype'] . "</td>";
+                echo "<td>" . $row['filename'] . "</td>";
+                echo "<td>" . $row['serieId'] . "</td>";
+                echo "<td>" . $row['date'] . "</td>";
+                echo "<td>" . $row['tempCCD'] . "</td>";
+                echo "<td>" . $row['binning'] . "</td>";
+                echo "<td>" . $row['detector'] . "</td>";
+            echo "</tr>\n";
+        }
+        echo "</table>";
+        echo '</form>';
+        echo $rowcount." files.<br>";
+
+        // Close result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+echo "<h2>Reduced spectrums</h2>";
 $sql = "select * from fileSpectrum where obsId=".$_GET['obsId']." ORDER by dateObs,orderNo";
-echo $sql;
+//echo $sql;
 if($result = mysqli_query($link, $sql)){
     
     $rowcount=mysqli_num_rows($result);
     
-    echo "<br>".$rowcount." files entry.<br>";
         
     if($rowcount > 0){
         
@@ -108,6 +165,8 @@ if($result = mysqli_query($link, $sql)){
         }
         echo "</table>";
         echo '</form>';
+        echo $rowcount." files.<br>";
+
         // Close result set
         mysqli_free_result($result);
     } else{

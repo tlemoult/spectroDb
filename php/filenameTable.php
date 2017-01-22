d4788abf54062e3cd7aa715894e2df45d16077ae
Thierry Lemoult <thierry.lemoult@eileo.com>
2017-01-22 23:55:29 +0100

    improved presentation

--- php/filenameTable.php
+++ php/filenameTable.php
@@ -19,14 +19,79 @@ if(empty($_GET['obsId']))
     echo "use  filenameTable?obsId=...<br>";
     exit;
 }
-else
-{
 
-    echo "<H1>Observation Id=".$_GET['obsId']."</H2>";
+///////////////////////////////////////////////////////////////
+echo "<H1>Observation page</H2>";
+$sql = "select observation.obsId,observation.dateObs,
+observer.alias as observer,
+site.name as siteName,
+object.name as obj,
+object.bayerName as obj2,
+object.OTYPE_V as objtype,
+object.SP_TYPE as objsptype,
+object.FLUX_V as magV,
+instrum.name as instr,observation.status,
+project.name as projName,
+observation.ref as isRef
+from observation
+left join object on object.objectId=observation.objId
+left join instrum on instrum.instruId=observation.instruId
+left join site on site.siteId=observation.siteId
+left join observer on observer.observerID=observation.observerId
+left join project on project.projectId =observation.projectId
+where observation.obsId=".$_GET['obsId'];
 
-	echo "<H2>Raw files</H2>";
-
+if($result = mysqli_query($link, $sql)){
+    
+    $rowcount=mysqli_num_rows($result);
+        
+    if($rowcount > 0){
+//      echo "nb result=" . $result . "<br>";
+      
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>obsId</th>";
+                echo "<th>dateObs (UTC)</th>";
+                echo "<th>site</th>";
+                echo "<th>project</th>";
+                echo "<th>REF</th>";         
+                echo "<th>target name</th>";
+                echo "<th>bayer name</th>";
+                echo "<th>target description</th>";
+                echo "<th>SpType</th>";            
+                echo "<th>MagV</th>";            
+                echo "<th>instrument</th>";
+                echo "<th>status</th>";
+            echo "</tr>\n";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['obsId'] . "</td>";
+                echo "<td>" . $row['dateObs'] . "</td>";
+                echo "<td>" . $row['siteName'] . "</td>";
+                echo "<td>" . $row['projName'] . "</td>";
+                echo "<td>" . $row['isRef'] . "</td>";
+                echo "<td>" . $row['obj'] . "</td>";
+                echo "<td>" . $row['obj2'] . "</td>";
+                echo "<td>" . $row['objtype'] . "</td>";
+                echo "<td>" . $row['objsptype'] . "</td>";
+                echo "<td>" . $row['magV'] . "</td>";
+                echo "<td>" . $row['instr'] . "</td>";
+                echo "<td>" . $row['status']. "</td>";                
+            echo "</tr>\n";
+        }
+        echo "</table>\n";
+        // Close result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
+
+
+echo "<H2>Acquisition raw files</H2>";
+
 
 // Attempt select query execution
 $sql = "select * from fileName where obsId=".$_GET['obsId']." AND phase='RAW'ORDER by date,filetype,filename";
@@ -46,6 +111,7 @@ if($result = mysqli_query($link, $sql)){
   				echo "<th>fileId</th>";
                 echo "<th>phase</th>";
                 echo "<th>filetype</th>";
+                echo "<th>path</th>";
                 echo "<th>filename</th>";
                 echo "<th>dateObs(UTC)</th>";
                 echo "<th>serieId</th>";         
@@ -58,6 +124,7 @@ if($result = mysqli_query($link, $sql)){
                 echo "<td>" . $row['fileId'] . "</td>";				
                 echo "<td>" . $row['phase'] . "</td>";
                 echo "<td>" . $row['filetype'] . "</td>";
+                echo "<td>" . $row['destDir'] . "</td>";                
                 echo "<td>" . $row['filename'] . "</td>";
                 echo "<td>" . $row['date'] . "</td>";
                 echo "<td>" . $row['serieId'] . "</td>";
@@ -79,7 +146,7 @@ if($result = mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
 
-echo "<H2>Other files </H2>";
+echo "<H2>Processing files</H2>";
 
 // Attempt select query execution
 $sql = "select * from fileName where obsId=".$_GET['obsId']." AND NOT phase='RAW'  ORDER by date,filetype,filename";
@@ -98,6 +165,7 @@ if($result = mysqli_query($link, $sql)){
                 echo "<th>fileId</th>";
                 echo "<th>phase</th>";
                 echo "<th>filetype</th>";
+                echo "<th>path</th>";
                 echo "<th>filename</th>";
                 echo "<th>date</th>";
                       
@@ -107,6 +175,7 @@ if($result = mysqli_query($link, $sql)){
                 echo "<td>" . $row['fileId'] . "</td>";             
                 echo "<td>" . $row['phase'] . "</td>";
                 echo "<td>" . $row['filetype'] . "</td>";
+                echo "<td>" . $row['destDir'] . "</td>";
                 echo "<td>" . $row['filename'] . "</td>";
                 echo "<td>" . $row['date'] . "</td>";
             echo "</tr>\n";
@@ -124,7 +193,7 @@ if($result = mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
 
-echo "<h2>Reduced spectrums</h2>";
+echo "<h2>Spectrums files</h2>";
 $sql = "select * from fileSpectrum where obsId=".$_GET['obsId']." ORDER by dateObs,orderNo";
 //echo $sql;
 if($result = mysqli_query($link, $sql)){
@@ -138,6 +207,7 @@ if($result = mysqli_query($link, $sql)){
         echo "\n<table border=2>";
             echo "<tr>";
                 echo "<th>fileId</th>";
+                echo "<th>path</th>";                
                 echo "<th>filename</th>";
                 echo "<th>dateObs(UTC)</th>";
                 echo "<th>exposure(s)</th>";
@@ -148,6 +218,7 @@ if($result = mysqli_query($link, $sql)){
         while($row = mysqli_fetch_array($result)){
             echo "<tr>";
                 echo "<td>" . $row['fileId'] . "</td>";             
+                echo "<td>" . $row['path'] . "</td>";
                 echo "<td>" . $row['filename'] . "</td>";
                 echo "<td>" . $row['dateObs'] . "</td>";
                 echo "<td>" . $row['expTime'] . "</td>";

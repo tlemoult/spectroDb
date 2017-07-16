ea4d2160930ac8b3ecf50d96807f409eb9cb33d3
tlemoult <thierry.lemoult@eileo.com>
2017-07-16 23:07:26 +0200

    first command line acquire with observation.json compatible with pipeline

--- telescope/indi/acquire.py
+++ telescope/indi/acquire.py
@@ -1,171 +1,83 @@
-import sys,time, logging,json
+import os,sys,time, datetime,logging,json
 import PyIndi
 
-
-class IndiClient(PyIndi.BaseClient):
-    deviceName=""
-    device = None
-    filePath= "."
-    fileNameRoot= "OBJECT-"
-    currentIndex= 1
-    expTime=1
-    serieRun=False
-    setPointTemperature=0
-    ccdTemperature=+32000
-
-    def __init__(self,deviceName):
-        super(IndiClient, self).__init__()
-        self.logger = logging.getLogger('PyQtIndi.IndiClient')        
-        self.logger.info('creating an instance of PyQtIndi.IndiClient')
-        self.deviceName=deviceName
-
-    def newAcquSerie(self,filePath,fileNameRoot,nbExposure,expTime):
-        self.logger.info("<<<<<<< New acquisition serie fileName=%s nbExposure=%d, expTime=%d"%(fileNameRoot,nbExposure,expTime))
-        self.currentIndex= 1
-        self.filePath=filePath
-        self.fileNameRoot=fileNameRoot
-        self.nbExposure=nbExposure
-        self.expTime=expTime
-        self.serieRun=True
-        self.takeExposure(expTime)
-
-    def newDevice(self, d):
-        self.logger.info("new device " + d.getDeviceName())
-        if d.getDeviceName() == self.deviceName:
-            self.logger.info("Found target device =%s"%self.deviceName)
-            # save reference to the device in member variable
-            self.device = d
-
-    def newProperty(self, p):
-        self.logger.info("new property "+ p.getName() + " for device "+ p.getDeviceName())
-        if self.device is not None and p.getName() == "CONNECTION" and p.getDeviceName() == self.device.getDeviceName():
-            self.logger.info("Got property CONNECTION for CCD Simulator!")
-            # connect to device
-            self.connectDevice(self.device.getDeviceName())
-            # set BLOB mode to BLOB_ALSO
-            self.setBLOBMode(1, self.device.getDeviceName(), None)
-        if p.getName() == "CCD_EXPOSURE":
-            self.logger.info("Got CCD_Exposure Property")
-            pass
-
-        if p.getName() == "CCD_TEMPERATURE":
-            self.logger.info("Got temperature Property")
-
-    def removeProperty(self, p):
-        self.logger.info("remove property "+ p.getName() + " for device "+ p.getDeviceName())
-
-    def newBLOB(self, bp):
-        self.logger.info("new BLOB "+ bp.name.decode())
-        # get image data
-        img = bp.getblobdata()
-        import cStringIO
-        # write image data to StringIO buffer
-        blobfile = cStringIO.StringIO(img)
-        # open a file and save buffer to disk
-        fullPath=self.filePath+'/'+self.fileNameRoot+str(self.currentIndex)+".fits"
-        self.logger.info("save file to "+fullPath)
-        with open(fullPath, "wb") as f:
-            f.write(blobfile.getvalue())
-        # start new exposure for acqusition serie 
-        self.currentIndex+=1
-        if (self.currentIndex<=self.nbExposure):
-            self.takeExposure(self.expTime)
-            self.serieRun=True
-        else:
-            self.serieRun=False
-        
-    def newSwitch(self, svp):
-        self.logger.info ("new Switch "+ svp.name.decode()  +" for device "+ svp.device.decode())
-    def newNumber(self, nvp):
-        self.logger.info("new Number "+ nvp.name.decode() + " value= %.2f"%(nvp[0].value)+ " for device "+ nvp.device.decode())
-        if nvp.name.decode()=="CCD_TEMPERATURE":
-            self.ccdTemperature=nvp[0].value
-
-        if nvp.name.decode()=="CCD_EXPOSURE":
-            self.ccdExposure=nvp[0].value
-
-    def newText(self, tvp):
-        self.logger.info("new Text "+ tvp.name.decode() + " for device "+ tvp.device.decode())
-    def newLight(self, lvp):
-        self.logger.info("new Light "+ lvp.name.decode() + " for device "+ lvp.device.decode())
-    def newMessage(self, d, m):
-        self.logger.info("new Message "+ d.messageQueue(m).decode())
-    def serverConnected(self):
-        print("Server connected ("+self.getHost()+":"+str(self.getPort())+")")
-    def serverDisconnected(self, code):
-        self.logger.info("Server disconnected (exit code = "+str(code)+","+str(self.getHost())+":"+str(self.getPort())+")")
+from lib.CamSpectro import IndiClient as CamSpectro
 
 
-    def takeExposure(self,expTime):
-        self.logger.info("<<<<<<<< Take Exposure, duration=%.2f >>>>>>>>>"%(expTime))
-        #get current exposure time
-        exp = self.device.getNumber("CCD_EXPOSURE")
-        # set exposure time to 5 seconds
-        exp[0].value = expTime
-        # send new exposure time to server/device
-        self.sendNewNumber(exp)
-        self.ccdExposure=expTime
-
-    def setTemperature(self,setPointTemperature):
-        self.setPointTemperature=setPointTemperature
-        self.logger.info("<<<<<<<<< Set Temperature set point =%.2f >>>>>>>>"%setPointTemperature)
-        t=self.device.getNumber("CCD_TEMPERATURE")
-        t[0].value = setPointTemperature
-        self.sendNewNumber(t)
-
-    def isCCDTemperatureOK(self):
-        e=self.ccdTemperature-self.setPointTemperature
-        self.logger.info("ccdTemp=%.1f  setPoint=%.1f  err=%.1f"%(self.ccdTemperature,self.setPointTemperature,e))
-        return abs(e)<2.0
-
-    def setBinning(self,binning):
-        self.binning=binning
-        self.logger.info("<<<<<<< Set binning to X=%d Y=%d"%(binning['X'],binning['Y']))
-        b=self.device.getNumber("CCD_BINNING")
-        b[0].value=binning['X']
-        b[1].value=binning['Y']
-        self.sendNewNumber(b)
-
-def waitEndAcqSerie(indiclient):
-    while indiclient.serieRun==True:
-        print """Exposure: %d/%d "%s" %.1f/%.1f seconds."""%( indiclient.currentIndex, indiclient.nbExposure  ,indiclient.fileNameRoot,indiclient.expTime-indiclient.ccdExposure,indiclient.expTime)
-        time.sleep(1)
+n=len(sys.argv)
+print "n=",n
+print sys.argv
+if n!=5:
+    print "syntaxe:"
+    print '    python acquire.py "Project" "OBJ NAME" nbExposure expTime'
+    print 'exemple:'
+    print '    python acquire.py "none"    "HD2203"   3           600'
+    exit()
+else:
+    projectName=sys.argv[1].split('"')[0]
+    print "projectName=",projectName
+    objName=sys.argv[2].split('"')[0]
+    print "objName=",objName
+    nbExposure=int(sys.argv[3])
+    print "nbExposure=",nbExposure
+    expTime=float(sys.argv[4])
+    print "expTime=",expTime
 
 #load configuration
 json_text=open('./configAcquire.json').read()
 config=json.loads(json_text)
-print config
 # setup log file
 logging.basicConfig(filename=config['logFile'],level=logging.DEBUG,format='%(asctime)s %(message)s')
+
+#create directory
+basePath=config['path']['acquire']+'/'+str(datetime.datetime.now()).replace(' ','_')
+print "basePath=",basePath
+os.mkdir(basePath)
+
+#create observation.json
+observationJson=config['templateObservation']
+observationJson['target']['objname']=[objName]
+observationJson['project']=projectName
+observationJson['target']['coord']['ra']=""
+observationJson['target']['coord']['dec']=""
+observationJson['statusObs']="started"
+observationJson['obsConfig']['NbExposure']=nbExposure
+observationJson['obsConfig']['ExposureTime']=expTime
+observationJson['obsConfig']['TotalExposure']=nbExposure*expTime
+
+with open(basePath+'/observation.json', 'w') as outfile:
+    json.dump(observationJson, outfile)
+
 # instantiate the client, for camera
-indiclient=IndiClient(config['ccdSpectro']['name'])
+camSpectro=CamSpectro(config['ccdSpectro']['name'])
 # set indi server
 server=config['ccdSpectro']['server']
-indiclient.setServer(str(server['host']),server['port'])
+camSpectro.setServer(str(server['host']),server['port'])
 # connect to indi server
 print("Connecting and waiting 2secs")
-if (not(indiclient.connectServer())):
-     print("No indiserver running on "+indiclient.getHost()+":"+str(indiclient.getPort())+" - Try to run")
+if (not(camSpectro.connectServer())):
+     print("No indiserver running on "+camSpectro.getHost()+":"+str(camSpectro.getPort())+" - Try to run")
      print("  indiserver indi_simulator_ccd")
      sys.exit(1)
-time.sleep(1)
-
-#set binning
-indiclient.setBinning(config['ccdSpectro']['binning'])
-#set temperature of CCD
-print("setTemperature")
-indiclient.setTemperature(config['ccdSpectro']['tempSetPoint'])
-while not indiclient.isCCDTemperatureOK():
-    print "   CCD temperature=%.1f"%indiclient.ccdTemperature
-    time.sleep(2)
+time.sleep(2)
 
 #acquisition
 print "run acquisition" 
-indiclient.newAcquSerie(config['path']['acquire'],"OBJECT-",4,10)
-waitEndAcqSerie(indiclient)
+camSpectro.newAcquSerie(basePath,"OBJECT-",nbExposure,expTime)
+camSpectro.waitEndAcqSerie()
 
-print "run acquisition" 
-indiclient.newAcquSerie(config['path']['acquire'],"FLAT-",4,1)
-waitEndAcqSerie(indiclient)
+raw_input('Switch on Neon, Press enter to continue: ')
+camSpectro.newAcquSerie(basePath,"NEON-",1,5)
+camSpectro.waitEndAcqSerie()
+
+#update json file
+observationJson['obsConfig']['NbExposure']=nbExposure
+observationJson['obsConfig']['ExposureTime']=expTime
+observationJson['obsConfig']['TotalExposure']=nbExposure*expTime
+observationJson['statusObs']="finished"
+with open(basePath+'/observation.json', 'w') as outfile:
+    json.dump(observationJson, outfile)
 
 print("acquisition finished")
+
+raw_input('Switch off Neon, Press enter to continue: ')

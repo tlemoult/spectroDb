e9e16d140a0f437afc40a3f89c0e17e422be71e7
Thierry Lemoult <thierry.lemoult@eileo.com>
2017-07-19 12:05:02 +0200

    creation, explication des bonne version pour astroplan

---
+++ python/installation.sh
@@ -0,0 +1,24 @@
+
+pip install astroquery
+pip install astroplan
+Matplotlib
+
+# bricoler les version
+pip uninstall astropy
+
+pip install numpy==1.11.2
+pip install astropy==1.2.1
+pip install astroplan==0.2.
+
+
+#verifier astroplan, afaire dans interpreteur python:
+import astroplan
+astroplan.test()
+
+
+
+
+
+
+
+

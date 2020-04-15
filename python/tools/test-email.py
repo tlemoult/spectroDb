d05ce40662923617caef0bd030055a7ca3c2bb63
Unknown <thierry.lemoult@eileo.com>
2020-04-15 17:53:29 +0200

    process spectrum with ISIS

--- python/tools/test-email.py
+++ python/tools/test-email.py
@@ -2,8 +2,8 @@ import lib.emailFnc as emailFnc
 import lib.dbSpectro as dbSpectro
 
 db=dbSpectro.init_connection()
-print dbSpectro.getProjectFollowers_fromProjectName(db,'RR_lyr')
+print dbSpectro.getProjectFollowers_fromProjectName(db,'del_cep')
 
-emailFnc.sendEmail("sujet Test carl", "message de test de carl",db,'RR_lyr')
+emailFnc.sendEmail("sujet Test carl", "message de test de carl. Hey j'ai ma propre adresse maintenant.",db,'NewBe')
 
 exit()

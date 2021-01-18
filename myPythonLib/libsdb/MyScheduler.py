de2532b8ac7825f83d80a2f017419f39eb3fe359
tlemoult <thierry.lemoult@eileo.com>
2021-01-18 23:10:38 +0100

    scheduler: format fix, request.lst

--- myPythonLib/libsdb/MyScheduler.py
+++ myPythonLib/libsdb/MyScheduler.py
@@ -57,11 +57,18 @@ class MySchedule(object):
 		print(self.priority_schedule)
 		print("**************************")
 	
+	def writeLstRequest(self,filename):
+		print("writeLstRequest:")
+		self.writeLst(filename,self.blocks)
 
-	def writeLst(self,filename):
+	def writeLstSchedule(self,filename):
+		print("writeLstSchedule:")
+		self.writeLst(filename,self.priority_schedule.observing_blocks)
+
+	def writeLst(self,filename,lstBlocks):
 		print("mySchedule.writeLst ",filename)
 		f=open(filename,'w+')
-		for block in self.priority_schedule.observing_blocks:
+		for block in lstBlocks:
 			name=block.target.name
 			project=(block.configuration)['project']
 			FLUX_V=block.configuration['FLUX_V']
@@ -73,14 +80,16 @@ class MySchedule(object):
 			extraConfig=block.configuration['extraConf']
 			calib=block.configuration['Calib']
 			
-			block.start_time.format = 'isot'
 
 			if name!='TransitionBlock':
 				coord=block.target.coord.to_string('hmsdms')
 				(ra,dec)=coord.split(' ')
-				print("name=%s  ra=%s  dec=%s"%(name,ra,dec))
-				line='"'+name+'"  '+ra[:-3]+'s  '+dec[:-3]+'s  '+str(FLUX_V)+'  FALSE  "Project='+str(project)
-				line+="&uid="+str(uid)
+				#fix cordinnate ra,dec precision to 2 digits
+				raLst=ra[:ra.find('m')]+'m'+"{:05.2f}".format(float(ra[ra.find('m')+1:ra.find('s')]))+"s"
+				decLst=dec[:dec.find('m')]+'m'+"{:05.2f}".format(float(dec[dec.find('m')+1:dec.find('s')]))+"s"
+				print(f"name={name} ra={ra} dec={dec} => raLst={raLst} decLst={decLst}")
+				line='"'+name+'"  '+raLst+'  '+decLst+'  '
+				line+=str(FLUX_V)+'  FALSE  "Project='+str(project)
 				line+="&Calib="+str(calib)
 				line+="&uid="+str(uid)
 				if ExposureTime!=None:  line+="&ExposureTime="+str(ExposureTime)
@@ -89,7 +98,11 @@ class MySchedule(object):
 				if intTime!=None: line+="&intTime="+str(intTime)
 				if extraConfig!=None: line+="&"+extraConfig
 
-				line+="&dateStart="+str(block.start_time)
+				if block.start_time is not None:
+					block.start_time.format = 'isot'
+					line+="&dateStart="+str(block.start_time)
+				else:
+					line+="&priority="+str(block.priority)
 				line+='"'
 				f.write(line+'\n')
 

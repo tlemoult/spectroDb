eef43a9639fc0f8a3e10377fe4785118cc33a3aa
Unknown <thierry.lemoult@eileo.com>
2021-01-19 09:19:09 +0100

    script request newBe

---
+++ scheduler/gen-newBe.sh
@@ -0,0 +1,16 @@
+
+echo genere programme observation nouvelle Be
+
+expTime=3600
+priority=2
+project=NewBe
+
+for star in 'BD+28 4177' 'BD+54 2775' 'BD+57 2581' 'BD+58 310' 'BD+59 2675' 'BD+60 133' 'BD+60 2523' 'BD+60 2645' 'BD+63 1964' 'HD12243' 'HD16485' 'HD179793' 'HD191494' 'HD225859' 'HD237126' 'HD253928' 'HD44783' 'BD+64 106' 'HD151067' 'BD+03 3861' 'HD34906' 'HD40724' 'HD9878' 'HD184061'
+do
+    echo $star
+    python3 in-request.py $project "$star" $priority $expTime
+    
+done
+
+
+#python3 in-request.py NewBe 'BD+28 4177' 1 3600

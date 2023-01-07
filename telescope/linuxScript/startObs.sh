452b11b620c89e956aaa6da37785a79499d57949
Thierry Lemoult <thierry.lemoult@gmail.com>
2023-01-07 22:14:21 +0100

    add lodestar ccd driver

--- telescope/linuxScript/startObs.sh
+++ telescope/linuxScript/startObs.sh
@@ -4,5 +4,5 @@ echo startup observatory
 cd ../python/
 python startupObs.py
 
-indiserver -v indi_lx200ap  indi_atik_ccd indi_asi_ccd
+indiserver -v indi_lx200ap  indi_atik_ccd indi_asi_ccd indi_sx_ccd
 

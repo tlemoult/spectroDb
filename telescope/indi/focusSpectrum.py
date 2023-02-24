ada19606a6554aed89027a49a21f15cf84694608
Thierry Lemoult <thierry.lemoult@gmail.com>
2023-02-24 09:09:50 +0100

    focus process, scan position and plot spectrum

--- telescope/indi/focusSpectrum.py
+++ telescope/indi/focusSpectrum.py
@@ -1,4 +1,4 @@
-import sys,time, logging,json
+import sys,time,os, logging,json
 import PyIndi
 from astropy.io import fits
 from scipy import ndimage
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from libindi.camera import CameraClient as CamSpectro
+from libobs import astrosib as astrosib
+from libcalc.img import statSpectrum
 
 #load configuration
 spectro_config = os.environ['SPECTROCONFIG']
@@ -13,38 +15,70 @@ configFilePath = os.path.join(spectro_config,'acquire.json')
 json_text=open(configFilePath).read()
 config = json.loads(json_text)
 
-path= config['path']['root']+'/'+config['path']['focus']
+path= config['path']['root']+config['path']['focus']
 
 # setup log file
-logging.basicConfig(filename=config['logFile'],level=logging.DEBUG,format='%(asctime)s %(message)s')
+pathFileNameLog = config['path']['root']+config['path']['log']+'/focus.log'
+print(f"Focus process, logpath = {pathFileNameLog}")
+logging.basicConfig(filename=pathFileNameLog,level=logging.WARNING,format='%(asctime)s %(message)s')
 # create and connect to Camera 
 camSpectro=CamSpectro(config["ccdSpectro"])
-camSpectro.setBinning({'X':3,'Y':3})
+#camSpectro.setBinning({'X':1,'Y':1})
 
 #acquisition
-print("run acquisition")
-exposureTime = 10
-camSpectro.newAcquSerie(path,"FOCUS-",1,exposureTime)
-camSpectro.waitEndAcqSerie()
-print("acquisition finished")
+exposureTime = 5
+focusAmplitude=2000
+focusNbPoint=8
 
-# stat
-filename = path+"/FOCUS-1.fits"
-hdulist = fits.open(filename)
-print(hdulist.info())
-print("clean hot pixel with median filter")
-img=ndimage.median_filter(hdulist[0].data,3)
+focusOrigin = astrosib.get_focus()
+print(f"Original focus Position = {focusOrigin}")
+focusStart = int(focusOrigin-focusAmplitude/2)
+focusStop  = int(focusOrigin+focusAmplitude/2)
 
-spectrumLine = 346
-spectrumHeight = 25
-lineStart = spectrumLine - spectrumHeight//2
-lineStop = spectrumLine + spectrumHeight//2
-imgUse = img[lineStart:lineStop,150:800]
-print(f"Extract zone lines [{lineStart}:{lineStop}]")
-print(f"Mean = {np.mean(imgUse)}   Max = {np.max(imgUse)}")
+focusPosRange = range(focusStart,focusStop,focusAmplitude//focusNbPoint)
+print(f"We will test the following focus position: {list(focusPosRange)}")
+focusMeans = []
 
-
-plt.imshow(imgUse, cmap=plt.cm.gray) 
+plt.ion()
 plt.show()
 
+fig, (ax1, ax2) = plt.subplots(2,sharex=True)
+fig.suptitle('preview spectrum')
+ax1.set_title("2D image")
+ax2.set_title("binned profil")
+ax2.set_xlabel("pixel X coord")
 
+
+for focusPos in focusPosRange:
+    print(f"go to focus position {focusPos}")
+    astrosib.set_focus_abs(focusPos)
+
+    print("run acquisition")
+    camSpectro.newAcquSerie(path,"FOCUS-",1,exposureTime)
+    camSpectro.waitEndAcqSerie()
+    print("acquisition finished")
+
+    # stat
+    filename = path+"/FOCUS-1.fits"
+
+    stat = statSpectrum(filename,binnigHeight = 60)
+
+    print(f"Mean = {stat['mean']} Max = {stat['max']}")
+    focusMeans.append(stat['mean'])
+
+    ax1.imshow(stat['img'], cmap=plt.cm.gray) 
+    ax2.plot(stat['spectrum'])
+    plt.draw()
+    plt.pause(0.1)
+
+print(f"go back to orinal focus Position = {focusOrigin}")
+astrosib.set_focus_abs(focusOrigin)
+
+print(f"FocusRange:  {list(focusPosRange)}")
+print(f"means {focusMeans}")
+
+plt.plot(list(focusPosRange),focusMeans)
+plt.title("Means vs focus position")
+plt.show()
+plt.draw()
+plt.pause(5)
